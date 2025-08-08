aa9eb829b684caff4f5bd7a514ad1af49e3b55c4
Black Cat <rborole06@gmail.com>
2025-08-08 14:00:11 +0000

    Change login remote url pattern

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -1,12 +1,18 @@
 import { createModuleFederationConfig } from '@module-federation/modern-js';
 
-const loginCodespaceName = process.env.REMOTE_LOGIN_APP_CODESPACE_SUBDOMAIN;
-const loginPort = process.env.REMOTE_LOGIN_APP_PORT;
+// const loginCodespaceName = process.env.REMOTE_LOGIN_APP_CODESPACE_SUBDOMAIN;
+const remoteLoginURL = process.env.REMOTE_LOGIN_APP_URL;
+
+// const remoteLoginPort = process.env.REMOTE_LOGIN_APP_PORT;
 
 export default createModuleFederationConfig({
   name: 'host',
   remotes: {
-    loginRemote: `loginRemote@https://${loginCodespaceName}-${loginPort}.app.github.dev/static/mf-manifest.json`,
+    // Github codespace specific login remote url
+    // loginRemote: `loginRemote@https://${remoteLoginURL}-${remoteLoginPort}.app.github.dev/static/mf-manifest.json`,
+
+    // Vercel specific login remote url
+    loginRemote: `loginRemote@${remoteLoginURL}/static/mf-manifest.json`,
   },
   shared: {
     react: { singleton: true },
