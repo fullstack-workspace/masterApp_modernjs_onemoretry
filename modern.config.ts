e41f818366d78c6dcd4cd733183f2099a82e6c44
Black Cat <rborole06@gmail.com>
2025-08-11 05:04:35 +0000

    update master modern config

--- modern.config.ts
+++ modern.config.ts
@@ -1,13 +1,19 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
 import { moduleFederationPlugin } from '@module-federation/modern-js';
 
-const codespaceName = process.env.MASTER_APP_CODESPACE_SUBDOMAIN;
-const port = Number.parseInt(process.env.MASTER_APP_PORT || '3052', 10);
+// Github codespace specific
+// const codespaceName = process.env.MASTER_APP_CODESPACE_SUBDOMAIN;
+// const port = Number.parseInt(process.env.MASTER_APP_PORT || '3052', 10);
+
+// Vercel speicifc
+const assetPrefix = process.env.MASTER_APP_URL;
 
 export default defineConfig({
   dev: {
-    assetPrefix: `https://${codespaceName}-${port}.app.github.dev/`,
-    port: port,
+    // assetPrefix: `https://${codespaceName}-${port}.app.github.dev/`,
+    // port: port,
+
+    assetPrefix: assetPrefix,
   },
   runtime: {
     router: true,
