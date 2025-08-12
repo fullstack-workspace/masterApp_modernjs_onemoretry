201e53379c70d8aadbf712f271afb43c6c556d58
Black Cat <rborole06@gmail.com>
2025-08-12 12:34:28 +0000

    change config files

--- modern.config.ts
+++ modern.config.ts
@@ -2,8 +2,8 @@ import { appTools, defineConfig } from '@modern-js/app-tools';
 import { moduleFederationPlugin } from '@module-federation/modern-js';
 
 // Github codespace specific
-// const codespaceName = process.env.MASTER_APP_CODESPACE_SUBDOMAIN;
-// const port = Number.parseInt(process.env.MASTER_APP_PORT || '3052', 10);
+const codespaceName = process.env.MASTER_APP_CODESPACE_SUBDOMAIN;
+const port = Number.parseInt(process.env.MASTER_APP_PORT || '3052', 10);
 
 // Vercel speicifc
 const assetPrefix = process.env.MASTER_APP_URL;
--- module-federation.config.ts
+++ module-federation.config.ts
@@ -1,18 +1,19 @@
 import { createModuleFederationConfig } from '@module-federation/modern-js';
 
-// const loginCodespaceName = process.env.REMOTE_LOGIN_APP_CODESPACE_SUBDOMAIN;
-const remoteLoginURL = process.env.REMOTE_LOGIN_APP_URL;
+const loginAppNameForCodespace =
+  process.env.REMOTE_LOGIN_APP_CODESPACE_SUBDOMAIN;
+const remoteLoginAppPortForCodespace = process.env.REMOTE_LOGIN_APP_PORT;
 
-// const remoteLoginPort = process.env.REMOTE_LOGIN_APP_PORT;
+const remoteLoginURLForVercel = process.env.REMOTE_LOGIN_APP_URL;
 
 export default createModuleFederationConfig({
   name: 'host',
   remotes: {
     // Github codespace specific login remote url
-    // loginRemote: `loginRemote@https://${remoteLoginURL}-${remoteLoginPort}.app.github.dev/static/mf-manifest.json`,
+    // loginRemote: `loginRemote@https://${loginAppNameForCodespace}-${remoteLoginAppPortForCodespace}.app.github.dev/static/mf-manifest.json`,
 
     // Vercel specific login remote url
-    loginRemote: `loginRemote@https://${remoteLoginURL}/static/mf-manifest.json`,
+    loginRemote: `loginRemote@${remoteLoginURLForVercel}/static/mf-manifest.json`,
   },
   shared: {
     react: { singleton: true },
